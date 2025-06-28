8027caf8b69bae4e72eede7435521de47a74a072
Lim Shang Yi <shangyilim@gmail.com>
2025-06-28 12:35:39 +0000

    remove firebase project details

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,12 +10,7 @@ import { getFirestore } from "firebase/firestore"; // Import Firestore
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAhV83hnME4Bsr2N-AWr1CnRsnuf7q8rUY",
-  authDomain: "data-weaver-s5c6o.firebaseapp.com",
-  projectId: "data-weaver-s5c6o",
-  storageBucket: "data-weaver-s5c6o.firebasestorage.app",
-  messagingSenderId: "61794580576",
-  appId: "1:61794580576:web:1ed536b773c67f6e40ab05"
+ 
 };
 
 // Initialize Firebase
